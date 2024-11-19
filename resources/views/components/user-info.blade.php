c30f49cac2f4675023ffa2e11fb9655e1aef95e5
Terence Cusi <tertercusi@gmail.com>
2024-11-19 22:56:22 +0800

    improve ui

--- resources/views/components/user-info.blade.php
+++ resources/views/components/user-info.blade.php
@@ -1,6 +1,6 @@
 @props(['title', 'description'])
 
 <div class="flex flex-col py-2">
-    <span class="uppercase font-bold text-sm"> {{ $title }} </span>
+    <span class="uppercase font-bold text-xs"> {{ $title }} </span>
     <span> {{ $description }} </span>
 </div>
